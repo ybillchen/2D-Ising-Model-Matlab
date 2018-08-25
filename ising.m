f7cac139a980c9ffa1ada540a0c2c7c3a71e88db
Bill <33487119+EnthalpyBill@users.noreply.github.com>
2018-08-25 17:14:21 +0800

    Update ising.m
    Correct some mistakes.
    Simplify some expressions.

--- ising.m
+++ ising.m
@@ -1,8 +1,8 @@
-% b is a list that records all energy data.
-% mb is a list that records all magnetization data.
-% a is a list that records average energy data.
-% ma is a list that records average magnetization data.
-% c and mc are used to record temporary data.
+% b records all energy data.
+% mb records all magnetization data.
+% a records average energy data.
+% ma records average magnetization data.
+% c and mc record temporary data.
 a = [];
 b = [];
 c = [];
@@ -14,7 +14,7 @@ mc = [];
 times = 10;
 
 for w = 1 : 30
-    % T is the temporature, the program will sweep it from 1 to 10.
+    % The program will sweep the temperature from 1 to 10.
     T = 1 + (0.3 * w);
     
     for u = 1 : times
@@ -37,15 +37,15 @@ for w = 1 : 30
 end
 
 % Plot data.
-% Left: energy-temporature figure
+% Plot left figure: energy-temperature
 subplot(1 , 2 , 1);
 plot(a(: , 1) , a(: , 2));
 hold on
-% Right: magnetization-temporature figure
+% Plot right figure: magnetization-temperature
 subplot(1 , 2 , 2);
 plot(ma(: , 1) , ma(: , 2));
 
-% Output data into txt files.
+% Output data as .txt files.
 %dlmwrite('a.txt' , a , '\t');
 %dlmwrite('b.txt' , b , '\t');
 %dlmwrite('ma.txt' , ma , '\t');
